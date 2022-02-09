8fe3b43896ef61a413e098b70e8222c05b601e96
Gabriel Brito <iamgabrielbp@gmail.com>
2022-02-09 11:07:27 -0300

    initial section done

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -1,9 +1,11 @@
 import "./styles.scss";
 export const Input = () => {
   return (
+    <div className="container">
     <div className="input-div">
       <input placeholder="shorten a link here..." />
       <button>Shorten it!</button>
+    </div>
     </div>
   );
 };
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,15 +4,18 @@ import illustration_working from "../images/illustration-working.svg";
 import "../styles/homestyles.scss";
 export const HomePage = () => {
   return (
-    <>
+    <div className="home-page">
       <Header />
       <section id="initial-section">
-
+      <div className="initial-text">
+        <h1>More than just shorter links</h1>
+        <p>Build your brand’s recognition and get detailed insights on how your links are performing.</p>
+        <button>Get Started</button>
+        </div>
+        <img src={illustration_working} alt={illustration_working}/>
       </section>
       <Input></Input>
-      <section id="mid-section">
-        
-        </section>
-    </>
+      <section id="mid-section"></section>
+    </div>
   );
 };
